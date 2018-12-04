1ffe752731c5641cd11c8d2a263751338541c660
Berk Emre Sarıbaş <berksaribas@sabanciuniv.edu>
2018-12-04 16:51:08 +0300

    Make project up to date

---
+++ Assets/Scripts/SimulationController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA;
+using UMA.Examples;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimulationController : MonoBehaviour
+{
+	public GameObject studentPrefab;
+	public UMACrowd crowd;
+	
+	private List<GameObject> agents = new List<GameObject>();
+	// Use this for initialization
+	void Start ()
+	{
+		for(int i = 0; i <100; i++)
+		{
+			crowd.ResetSpawnPos();
+			var go = crowd.GenerateUMA(Random.Range(0, 2), transform.position);
+			go.transform.localRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+
+			UMAData umaData = go.GetComponent<UMAData>();
+			umaData.CharacterCreated.AddListener(CharacterCreated);
+
+			var lod = go.AddComponent<O3nUMASimpleLOD>();
+			lod.lodDistance = 4;
+			lod.swapSlots = true;
+			lod.lodOffset = 0;
+			lod.Update();
+			
+			agents.Add(go);
+		}
+	}
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Giving all agents a random destination");
+			foreach (var agent in agents)
+			{
+				var navMesh = agent.GetComponent<NavMeshAgent>();
+				var animator = agent.GetComponent<Animator>();
+				if (navMesh != null)
+				{
+					NavMeshHit closestHit;
+					if (NavMesh.SamplePosition(new Vector3(Random.Range(-30f, 30f), 0, Random.Range(-20f, 30f)),
+						out closestHit, 100f, NavMesh.AllAreas))
+					{
+						navMesh.SetDestination(closestHit.position);
+					}
+
+					animator.SetFloat("Speed", 0.11f);
+				}
+			}
+		}
+	}
+	
+	void CharacterCreated(UMAData umaData)
+	{
+		umaData.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+		umaData.gameObject.AddComponent<StudentBehaviour>();
+	}
+}
---
+++ Assets/Scripts/StudentBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StudentBehaviour : MonoBehaviour
+{
+
+	public NavMeshAgent agent;
+	public Animator animator;
+	
+	void Start ()
+	{
+		animator = GetComponent<UMAData>().animator;
+		transform.position = new Vector3(Random.Range(-28, 28f), 0, Random.Range(-16, 16f));
+		
+		NavMeshHit closestHit;
+		if (NavMesh.SamplePosition (transform.position, out closestHit, 100f, NavMesh.AllAreas)) {
+			transform.position = closestHit.position;
+			agent = gameObject.AddComponent<NavMeshAgent>();
+			agent.speed = 0.1f;
+		}
+	}
+}
